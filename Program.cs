4689fa69e97a1d1390414da7a03923beabebb874
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-11 14:31:10 -0500

    Fixed spelling and range

--- Program.cs
+++ Program.cs
@@ -10,13 +10,14 @@ namespace Topic_4___Random_Numbers
             int randomInteger, max, min, firstRoll, secondRoll,sumRolls, roundValue;
             double randomDouble, number;
 
-            //Random integer generator
+            //Random Integer Generator
             Console.WriteLine("Hey there! Give me a maximum value and a minimum value and I'll generate you five random numbers!\nMax number is: ");
             Int32.TryParse(Console.ReadLine(), out max);
             Console.WriteLine("Min number is: ");
             Int32.TryParse(Console.ReadLine(), out min);
             Console.WriteLine();
 
+            // Max/Min check
             if (max < min)
             {
                 Console.WriteLine("Not possible... The maximum is bigger than the minimum.");
@@ -30,7 +31,7 @@ namespace Topic_4___Random_Numbers
                     Console.Write(randomInteger + " ");
                 }
 
-                //Dice rolls
+                //Dice Rolls
                 Console.WriteLine();
                 Console.WriteLine("How about I roll some dice for you? ");
                 Console.WriteLine();
@@ -38,11 +39,11 @@ namespace Topic_4___Random_Numbers
                 firstRoll = generator.Next(1, 7);
                 secondRoll = generator.Next(1, 7);
                 sumRolls = firstRoll + secondRoll;
-                Console.WriteLine($"The first roll was {firstRoll} and the second roll was {secondRoll}. The sum of those two numbers is {sumRolls}.");
+                Console.WriteLine($"The first roll was a {firstRoll} and the second roll was a {secondRoll}. The sum of those two numbers is {sumRolls}.");
 
-                //Random Decimal generator
+                //Random Decimal Generator
                 Console.WriteLine();
-                Console.WriteLine("What about a decimal generator? I'll generate three random numbers with the maximum and minimm you gave me!\nHow many numbers should I round to?");
+                Console.WriteLine("What about a decimal generator? I'll generate three random numbers with the maximum and minimum you gave me!\nHow many decimals should I round to?");
                 Int32.TryParse(Console.ReadLine(), out roundValue);
 
                 Console.WriteLine();
@@ -50,7 +51,7 @@ namespace Topic_4___Random_Numbers
 
                 for (int i = 1; i <= 3; i++)
                 {
-                    randomInteger = generator.Next(min, max + 1);
+                    randomInteger = generator.Next(min, max);
                     randomDouble = generator.NextDouble();
                     number = randomDouble + randomInteger;
                     Console.Write(Math.Round(number, roundValue) + " ");
